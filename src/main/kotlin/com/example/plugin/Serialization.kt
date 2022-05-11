aedcaf22de46bc7d75f04ce336725d6a460be04b
Nyanki <nicognaw@outlook.com>
2022-05-11 23:56:51 +0800

    refactor: use gson instead of kotlinx.serialization.json

--- src/main/kotlin/com/example/plugin/Serialization.kt
+++ src/main/kotlin/com/example/plugin/Serialization.kt
@@ -1,15 +1,29 @@
 package com.example.plugin
 
-import io.ktor.serialization.kotlinx.json.*
+import com.example.common.response.ErrorResults
+import io.ktor.http.*
+import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
+import io.ktor.server.application.hooks.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.json.Json
+
 
 fun Application.configureSerialization() {
+    install(createApplicationPlugin("SerializationHandler") {
+        on(CallFailed) { call, cause ->
+            if (cause is BadRequestException) {
+                call.respond(status = HttpStatusCode.BadRequest, ErrorResults.InvalidArgument)
+            }
+        }
+    })
     install(ContentNegotiation) {
-        json(Json { encodeDefaults = false })
+//        json(Json { encodeDefaults = false })
+        gson {
+            setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        }
     }
 
     routing {
