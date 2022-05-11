d638f42077a242c268421ae524a928c094418d56
Nyanki <nicognaw@outlook.com>
2022-05-11 23:57:29 +0800

    feat: constant error results

---
+++ src/main/kotlin/com/example/common/response/ErrorResults.kt
@@ -0,0 +1,16 @@
+package com.example.common.response
+
+import com.example.common.vo.OperateError
+import com.example.common.vo.OperateResult
+
+object ErrorResults {
+    val NoCredentials = OperateResult(error = OperateError(title = "请提供账户名及密码", status = "NoCredentials"))
+
+    val InvalidCredentials = OperateResult(error = OperateError(title = "账户名或密码不正确", status = "InvalidCredentials"))
+
+    val SystemError = OperateResult(error = OperateError(title = "系统错误", status = "SystemError"))
+
+    val Identityless = OperateResult(error = OperateError("未登录，请先登录。", "Identityless"))
+
+    val InvalidArgument = OperateResult(error = OperateError("无效参数，请检查格式。", "InvalidArgument"))
+}
