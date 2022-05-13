ef9170a1373bcc43da92ef011daf022f9d712d1a
Nyanki <nicognaw@outlook.com>
2022-05-13 15:28:16 +0800

    feat: map based ordinary response

--- src/main/kotlin/com/example/common/vo/PageResponse.kt
+++ src/main/kotlin/com/example/common/vo/PageResponse.kt
@@ -3,6 +3,11 @@ package com.example.common.vo
 import io.ktor.server.application.*
 import io.ktor.util.pipeline.*
 
-inline fun <reified T> PipelineContext<*, ApplicationCall>.pageResponse(meta: Page, data: List<T>): Map<String, Any> {
-    return mapOf("meta" to meta, "data" to data)
-}
+inline fun <reified T> PipelineContext<*, ApplicationCall>.pageResponse(meta: Page, data: List<T>): Map<String, Any> =
+    ordinaryResponse(meta, data)
+
+
+inline fun <reified T> PipelineContext<*, ApplicationCall>.ordinaryResponse(
+    meta: Any,
+    data: List<T>
+): Map<String, Any> = mapOf("meta" to meta, "data" to data)
